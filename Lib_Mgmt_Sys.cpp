e60ccaa97de6c13b4a5bb3390e9d6284e93f32dd
harsh007-github <khandelwalharsh401@gmail.com>
2023-03-26 11:39:42 +0530

    Errors fixed

--- Lib_Mgmt_Sys.cpp
+++ Lib_Mgmt_Sys.cpp
@@ -1,50 +1,82 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 class library
 {
 	private:
-		bool issueBook = false;
+		bool issueBook;
 		string bookName;
-		vector<string>v;
-		v.push_back("power of habits");
-		v.push_back("atomic habit");
-		v.push_back("ikigai");
+		vector<string> v;
+
 	public:
+		library()
+		{
+			v.push_back("power of habits");
+			v.push_back("atomic habit");
+			v.push_back("ikigai");
+		}
+
 		void welcomeMsg()
 		{
-				cout << "Would you like to issue a book('1' for yes '0' for no):";
-				cin >> issueBook;
-				if(issueBook == true)
-				{
-					bookIssue();
-				}
-				else
-				{
-					endMsg();
-				}
+			cout << "Would you like to issue a book ('1' for yes, '0' for no): ";
+			cin >> issueBook;
+			if (issueBook)
+			{
+				bookList();
+			}
+			else
+			{
+				endMsg();
+			}
 		}
-		void endMsg()
+
+		void bookList()
 		{
-			cout << "Thank you for visiting";
+			cout << endl << "Here are the books available:" << endl;
+			for(int j = 0; j < v.size(); j++)
+			{
+				cout <<j + 1 <<"." << " "<< v[j] << endl;
+			}
+			bookIssue();
 		}
+		
 		void bookIssue()
 		{
-			cout << "Which Book Would You Like to Issue:";
-			getline(cin,bookName);
-			for(int i =0; i < v.size();i++)
+			
+			cout << "Which book would you like to issue: ";
+			cin.ignore();
+			getline(cin, bookName);
+
+			bool bookFound = false;
+
+			for (int i = 0; i < v.size(); i++)
 			{
-				if(v[i] == bookName)
+				if (v[i] == bookName)
 				{
-					cout << bookName <<"is issued to you";
-					return;
+					cout << bookName << " is issued to you." << endl;
+					bookFound = true;
+					break;
 				}
 			}
-			cout << "Book not found";
+
+			if (!bookFound)
+			{
+				cout << "Sorry, book not found in the library." << endl;
+			}
+		}
+		
+		void endMsg()
+		{
+			cout << "Thank you for visiting." << endl;
 		}
 };
+
 int main()
 {
 	library lib;
-	cout << "Welcome to Harsh's Library:" << endl;
+	cout << "Welcome to Harsh's Library." << endl;
 	lib.welcomeMsg();
+
+	return 0;
 }
+
